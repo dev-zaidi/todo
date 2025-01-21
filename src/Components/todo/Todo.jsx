a3937710bacc7cde7133235a115b8f61cc922f25
Hiba Areej <syedahibaareej@gmail.com>
2025-01-21 12:51:29 +0500

    Update Todo.jsx

--- src/Components/todo/Todo.jsx
+++ src/Components/todo/Todo.jsx
@@ -4,8 +4,8 @@ import { CiEdit } from "react-icons/ci";
 import { MdDeleteOutline, MdFileDownloadDone } from "react-icons/md";
 import Button from "../Buttons/Button";
 import Input from "../Input/Input";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import styles from './todo.module.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import styles from "./todo.module.css";
 
 const Todo = () => {
   const [task, setTask] = useState({ title: "", status: false });
@@ -59,7 +59,8 @@ const Todo = () => {
   });
 
   return (
-    <div className="container mt-4 text-center">
+    <div className='container text-center p-2'>
+      <div className={styles.todoBody}>
       <Input
         onchange={(e) => {
           setTask({ ...task, title: e.target.value });
@@ -68,54 +69,55 @@ const Todo = () => {
         placeholder="Enter Task"
         value={task.title}
       />
-      <Button color="green" icon={<IoIosAdd />} handleFunction={handleSubmit} />
+      <Button color="#00695c" icon={<IoIosAdd />} handleFunction={handleSubmit} />
 
-    
       <div className={styles.dropdown}>
-        <select
-          value={filter}
-          onChange={(e) => setFilter(e.target.value)}
-        >
+        <select value={filter} onChange={(e) => setFilter(e.target.value)}>
           <option value="all">All Tasks</option>
           <option value="pending">Pending Tasks</option>
           <option value="completed">Completed Tasks</option>
         </select>
         <Button
-          color="red"
+          color="#e53935"
           backgroundColor="white"
           icon={<MdDeleteOutline />}
-        //   label={`Delete All ${filter === "all" ? "Tasks" : filter === "pending" ? "Pending Tasks" : "Completed Tasks"}`}
+          //   label={`Delete All ${filter === "all" ? "Tasks" : filter === "pending" ? "Pending Tasks" : "Completed Tasks"}`}
           handleFunction={handleDeleteFilteredTasks}
         />
       </div>
 
-      <div className="mt-3">
-        
-      </div>
+      <div className="mt-3"></div>
 
       <div className="row mt-4">
         {filteredTodo.map((item, index) => (
           <div className="col-md-4 mb-3" key={index}>
-            <div className="card">
-              <div className="card-header">
-                <span className={`badge ${item.status ? "bg-success" : "bg-warning"} ms-2`}>
+            <div className="card border-success">
+              <div className="card-header border-success">
+                <span
+                  className={`badge ${
+                    item.status ? "text-success" : "text-warning"
+                  } ms-2`}
+                >
                   {item.status ? "Completed" : "Pending"}
                 </span>
               </div>
-              <p className="card-text">{item.title}</p>
-              <div className="card-body d-flex justify-content-between">
+              <p className="card-text m-1">{item.title}</p>
+              <div className="card-body d-flex justify-content-center">
                 <Button
-                  color="yellow"
+                  color="#ffc107"
+                  backgroundColor="transparent"
                   icon={<CiEdit />}
                   handleFunction={() => handleEdit(index)}
                 />
                 <Button
-                  color="red"
+                  color="#e53935"
+                  backgroundColor="transparent"
                   icon={<MdDeleteOutline />}
                   handleFunction={() => handleDelete(index)}
                 />
                 <Button
-                  color="green"
+                  color="#1b5e20"
+                  backgroundColor="transparent"
                   icon={<MdFileDownloadDone />}
                   handleFunction={() => handleComplete(index)}
                 />
@@ -124,7 +126,9 @@ const Todo = () => {
           </div>
         ))}
       </div>
-    </div>
+
+      </div>
+          </div>
   );
 };
 
