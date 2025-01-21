0e26e106532a4dc2d40de0b44aa3df408319e72f
Hiba Areej <syedahibaareej@gmail.com>
2025-01-21 12:53:48 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import Todo from './Components/todo/Todo'
-import 'bootstrap/dist/css/bootstrap.min.css'
-
+import React from "react";
+import Todo from "./Components/todo/Todo";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Navbar from "./Components/Nav/Navbar";
 
 const App = () => {
   return (
     <>
-     <div className='container-fluid'>
-     <Todo/>
-     </div>
-      
-    </>    
-  )
-}
+      <div className="container">
+      <Navbar />
+      <Todo />
+      </div>
+    </>
+  );
+};
 
-export default App
+export default App;
